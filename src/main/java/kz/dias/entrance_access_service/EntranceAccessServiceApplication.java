131f04f10cc2b89cede568254a6fb0c45842b5be
dias-duisembayev <dias.duisembayev@gmail.com>
2023-04-08 18:56:29 +0600

    Init

---
+++ src/main/java/kz/dias/entrance_access_service/EntranceAccessServiceApplication.java
@@ -0,0 +1,13 @@
+package kz.dias.entrance_access_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EntranceAccessServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EntranceAccessServiceApplication.class, args);
+    }
+
+}
---
+++ src/test/java/kz/dias/entrance_access_service/EntranceAccessServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package kz.dias.entrance_access_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EntranceAccessServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
